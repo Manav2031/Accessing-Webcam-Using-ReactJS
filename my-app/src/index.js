c69a2e45935c0816565744827d83e19462e66e02
Manav2031 <manavmehta2031@gmail.com>
2024-03-05 19:42:06 +0530

    added .env file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,8 +7,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Auth0Provider
-     domain="dev-ovlz46pzx7j8dgbd.us.auth0.com"
-     clientId="R4UZybGpkM17z2wxG4R7EppAWxuFBVSE"
+     domain={process.env.REACT_APP_AUTH0_DOMAIN}
+     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
      authorizationParams={{
        redirect_uri: window.location.origin
      }}
