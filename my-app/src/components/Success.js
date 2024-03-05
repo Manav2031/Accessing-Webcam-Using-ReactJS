2c5b02719cde72b72e814eee7c9aa94b2cded4d2
Manav2031 <manavmehta2031@gmail.com>
2024-03-05 23:30:00 +0530

    made changes in design

--- my-app/src/components/Success.js
+++ my-app/src/components/Success.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from 'react';
+import '../styles/success.css';
+import Camera from './Camera';
+import FileUpload from './FileUpload';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Success = () => {
   const {logout} = useAuth0();
 
   return (
-    <>
-    <div>Success</div>
-    <br /> <br />
+    <div className="success">
+      <div className="containers">
+      <div className="leftcontainer">
+        <h1 className="lch1"> Asset </h1>
+        <Camera />
+        <br /> <br />
+      </div>
+      <div className="rightcontainer">
+        <h1 className="rch1"> Invoice </h1>
+        <br /> <br />
+        <FileUpload />
+        <br /> <br />
+      </div>
+      </div>
     <button onClick={() => logout()}> Logout </button>
-    </>
+    <br /> <br />
+    <br /> <br />
+  </div>
   )
 }
 
